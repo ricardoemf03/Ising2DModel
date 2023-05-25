8430f5cc4e7e62a82e0454a802e29e01ae791e6b
ricardoemf03 <ricardoemf03@gmail.com>
2023-05-25 17:39:30 -0600

    English version

--- isingModelv1.py
+++ isingModelv1.py
@@ -1,87 +1,93 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import random
-import copy
-from matplotlib.colors import ListedColormap #para colores blanco y negro de conf inicial y final
-cmap = ListedColormap(['k', 'w']) #para colores blanco y negro de conf inicial y final
-rdmsedd = random.seed(24)#semilla de numero aleatorios
-rdd = np.random.seed(24) #semilla para numpy
-N = 15 # Tamaño de la red
-J = 1 # Constante J
-kb=1 #constante de Boltzmann
-nsteps = 200000 # Número de iteraciones de Monte Carlo
-# Crear la red inicial de espines aleatorios
+#Importing the necessary libraries and modules required for the program's functionality:
+
+import numpy as np #Support for efficient numerical operations and array manipulation.
+import matplotlib.pyplot as plt #To create charts, plots, and visual props.
+import random #This provides multiple functions for generating random numbers, shuffling sequences, and making random selections.
+import copy #Functions for creating shallow or deep copies of objects.
+from matplotlib.colors import ListedColormap #For the black and white colors of the initial and final configurations.
+cmap = ListedColormap(['k', 'w'])
+
+#Define the parameters of the system:
+
+rdmsedd = random.seed(24)#Random number seed.
+rdd = np.random.seed(24) #Seed for NumPy.
+N = 15 #Red size.
+J = 1 #Constant J.
+kb=1 #Boltzmann's constant.
+nsteps = 200000 #Number of Monte Carlo iterations.
+#Creating the initial lattice of random spins:
 spins = np.random.choice([-1, 1], size=(N, N))
-initial_conf = copy.deepcopy(spins) #copia de configuracion inicial para luego mostrar
-#definir la funcion que calcula la energia
+initial_conf = copy.deepcopy(spins) #Copy of the initial configuration to display later.
+#Defining the function that calculates the energy:
 def energy(spins):
-    """Calcula la energía de la red de espines"""
-    # Calcular la suma de los productos de los espines adyacentes
+    """Calculates the energy of the spin lattice"""
+    #Calculating the sum of the products of adjacent spins:
     neighbors = np.roll(spins, 1, axis=0) + np.roll(spins, -1, axis=0) + np.roll(spins, 1, axis=1) + np.roll(spins, -1, axis=1)
     energy = -J * np.sum(spins * neighbors/2)
     return energy
 
-# Definir la función de magnetización
+#Defining the magnetization function:
 def magnetization(spins):
     return np.sum(spins)
 
-# Lista de temperaturas
+#Temperature list:
 temperatures = np.linspace(0.1, 5.0, 90)
 
-avg_energies = [] # Lista de energías promedio para cada T
-heat_cap = [] # Lista de capacidades calorificas promedio para cada T
-avg_mag = [] # Lista de magnetizaciones promedio para cada T
-sus_mag = [] # Lista de suceptibilidades magneticas promedio para cada T
+#Creating empty lists to store the average energies, heat capacities, average magnetizations, and susceptibility of magnetization:
+avg_energies = [] #List of average energies for each T.
+heat_cap = [] #List of average heat capacities for each T.
+avg_mag = [] #List of average magnetizations for each T.
+sus_mag = [] #List of average magnetic susceptibilities for each T.
 for T in temperatures:
-    # Energías de Monte Carlo para esta temperatura
+    #Monte Carlo energies for this temperature:
     energies = []
-    # Magnetizaciones de monte carlo para esta temperatura
+    #Monte Carlo magnetizations for this temperature:
     magnetizations = []
     for step in range(nsteps):
-        # Seleccionar un espín aleatorio
+        #Select a random spin:
         i, j = random.randint(0, N-1), random.randint(0, N-1)
-        # Calcular la energía del estado actual
+        #Calculate the energy of the current state:
         E = energy(spins)
-        #Calcular la magnetizacion del estado actual
+        #Calculate the magnetization of the current state:
         M = magnetization(spins)
-        # Cambiar el espín
+        #Changing the spin:
         spins[i, j] *= -1
-        # Calcular la energía del nuevo estado
+        #Calculate the energy of the new state:
         Enew = energy(spins)
-        #Caclular magnetizacion del nuevo estado
+        #Calculate magnetization of the new state:
         Mnew = magnetization(spins)
-        # Calcular la diferencia de energía
+        #Calculate the energy difference:
         deltaE = Enew - E
-        # Aceptar o rechazar el cambio con la probabilidad adecuada
+        #Accept or reject the change with the appropriate probability based on the Metropolis criterion:
         if deltaE <= 0 or np.exp(-deltaE /(kb*T)) > random.uniform(0, 1):
             energies.append(Enew)
             magnetizations.append(Mnew)
         else:
-            #Revertir el cambio
+            #Revert the change:
             spins[i, j] *= -1
             energies.append(E)
             magnetizations.append(M)
-    # Energía promedio para esta temperatura
+    #Average energy for this temperature:
     avg_energies.append(2*np.mean(energies)/N**2)
-    # Magnetizacion promedio para esta temperatura
+    #Average magnetization for this temperature:
     avg_mag.append(np.mean(magnetizations)/N**2)
-    sqrt_energies = [ener**2 for ener in energies] # cuadrado de las energias
-    Eprom = np.mean(energies) # <E> para esta T
-    E2prom = np.mean(sqrt_energies) # <E^2> para esta T
-    # capacidad calorifica promedio para esta temperatura
+    sqrt_energies = [ener**2 for ener in energies] #The square of the energies.
+    Eprom = np.mean(energies) #<E> for this T.
+    E2prom = np.mean(sqrt_energies) #<E^2> for this T.
+    #Average heat capacity for this temperature:
     heat_cap.append((E2prom-Eprom**2)/(N**2*kb*T**2))
-    sqrt_mag = [mags**2 for mags in magnetizations] # cuadrado de las magnetizaciones
-    AbsMag = [abs(mm) for mm in magnetizations] # valor absoluto de las magnetizaciones
-    Mprom = np.mean(AbsMag) # <|M|>
-    M2prom = np.mean(sqrt_mag) # <M^2>
-    # susceptibilidad magnetica promedio para esta temperatura
+    sqrt_mag = [mags**2 for mags in magnetizations] #Square of the magnetizations.
+    AbsMag = [abs(mm) for mm in magnetizations] #Absolute value of the magnetizations.
+    Mprom = np.mean(AbsMag) #<|M|>
+    M2prom = np.mean(sqrt_mag) #<M^2>
+    #Average magnetic susceptibility for this temperature:
     sus_mag.append((M2prom-Mprom**2)/(N**2*kb*T))
 
     print("para T= ", T, " Eprom = ", 2*np.mean(energies)/N**2, " Mprom = ", np.mean(magnetizations)/N**2 , " capcal= ", (E2prom-Eprom**2)/(N**2*kb*T**2), "sus = ", (M2prom-Mprom**2)/(N**2*kb*T))
-heat_cap[0]=0 # para que salga bien la grafica remplazamos el primer valor de heatcap ya que es muy grande
-sus_mag[0]=0 # de igual manera la susceptibilidad
-final_conf=copy.deepcopy(spins) # hacemos una copia de la configuracion final
-# Para mostrar los graficos juntos
+heat_cap[0]=0 #In order to obtain a correct graph, we replace the first value of heatcap since it is very large.
+sus_mag[0]=0 #Likewise the susceptibility.
+final_conf=copy.deepcopy(spins) #Making a copy of the final configuration.
+#To display the plots together:
 fig = plt.figure()
 fig.clf()
 ax = fig.subplots(2,3)
@@ -106,6 +112,9 @@ ax[1,1].set_xlabel('Temperature')
 ax[1,1].set_ylabel('Magnetic susceptibility')
 ax[1,1].set_title('Magnetic susceptibility vs Temperature')
 
+#Plot the configuration, using "plt.matshow": it visualizes a 2D array or matrix as a color-coded grid, where each cell's
+#color represents the corresponding value in the matrix.
+
 ax[0,2].matshow(initial_conf, cmap=cmap)
 ax[0,2].set_title('Initial configuration')
 
@@ -116,7 +125,7 @@ fig.tight_layout()
 fig.show()
 plt.show()
 
-# Para mostrar cada grafica por individual
+#To display each plot individually:
 plt.scatter(temperatures, avg_energies, color="black", s=10)
 plt.xlabel('Temperature')
 plt.ylabel('<E>')
